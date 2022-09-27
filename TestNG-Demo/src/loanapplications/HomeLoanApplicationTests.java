21fa5370bcf7874060f2061e236cd8f6dce9b92b
Radhakrishnan Meiappan <rmeiappan@rocketsoftware.com>
2022-09-27 11:34:02 +0530

    changes

---
+++ TestNG-Demo/src/loanapplications/HomeLoanApplicationTests.java
@@ -0,0 +1,21 @@
+package loanapplications;
+
+import org.testng.annotations.Test;
+
+public class HomeLoanApplicationTests {
+
+	@Test
+	public void HomeLoanLoginWeb() {
+		System.out.println("Home loan application - web login test");
+	}
+	
+	@Test
+	public void HomeLoanLoginMobile() {
+		System.out.println("Home loan application - mobile login test");
+	}
+	
+	@Test
+	public void HomeLoanLoginAPI() {
+		System.out.println("Home loan application - API login test");
+	}
+}
--- selenium-demo/src/com/basic/TestAngApp.java
+++ selenium-demo/src/com/basic/TestAngApp.java
@@ -17,7 +17,11 @@ public class TestAngApp {
 
 //		clickAccountsButton(webdriver);
 
-		addProductToShoppingCart(webdriver, "ecomm", "headphones", "2");
+//		addProductToShoppingCart(webdriver, "ecomm", "headphones", "2");
+		
+//		addProductToShoppingCartXPath(webdriver, "ecomm", "xpath", "3");
+		
+		addPosts(webdriver, "tforms", "x@y.com", "");
 
 //		webdriver.quit();
 
@@ -30,6 +34,27 @@ public class TestAngApp {
 		webdriver.findElement(By.name("addProduct")).click();
 		webdriver.findElement(By.name("productName")).sendKeys("");
 		webdriver.findElement(By.name("quantity")).sendKeys("");
+
+	}
+	
+	private static void addProductToShoppingCartXPath(WebDriver webdriver, String route, String productname, String quantity) {
+		webdriver.get(url + "/" + route);
+		webdriver.findElement(By.xpath("//input[@placeholder='Product Name']")).sendKeys(productname);
+		webdriver.findElement(By.cssSelector("input[placeholder='Quantity']")).sendKeys(quantity);
+		webdriver.findElement(By.name("addProduct")).click();
+		webdriver.findElement(By.name("productName")).sendKeys("");
+		webdriver.findElement(By.name("quantity")).sendKeys("");
+
+	}
+	
+	private static void addPosts(WebDriver webdriver, String route, String email, String quantity) {
+		webdriver.get(url + "/" + route);
+		webdriver.findElement(By.xpath("//form/div[2]/div/input")).sendKeys(email);
+//		webdriver.findElement(By.cssSelector("form input:nth-child(2)")).sendKeys(quantity);
+		webdriver.findElement(By.xpath("//form/input[2]")).sendKeys(quantity);
+//		webdriver.findElement(By.name("addProduct")).click();
+//		webdriver.findElement(By.name("productName")).sendKeys("");
+//		webdriver.findElement(By.name("quantity")).sendKeys("");
 
 	}
 
